f970e920e374908c7bb3bf8b67f7836a2255fdd6
yeskiy <eqwer24@gmail.com>
2026-06-28 15:47:29 +0300

    chore(deps): update example project dependencies to the latest
    Bumps every example to the latest versions, including the majors:
    @babel 7 to 8, webpack-cli 6 to 7, @rspack 1 to 2, and
    copy-webpack-plugin 13 to 14. node-webpack drops
    @babel/plugin-syntax-async-generators, which has no Babel 8 release and
    is redundant since Babel 8 parses async generators natively. The rollup
    example now ignores its generated package-lock.json like its siblings.

--- example/node-webpack/webpack.config.js
+++ example/node-webpack/webpack.config.js
@@ -25,7 +25,6 @@ module.exports = {
                     loader: "babel-loader",
                     options: {
                         presets: ["@babel/preset-env"],
-                        plugins: ["@babel/plugin-syntax-async-generators"],
                     },
                 },
             },
@@ -38,7 +37,6 @@ module.exports = {
                         loader: "babel-loader",
                         options: {
                             presets: ["@babel/preset-env"],
-                            plugins: ["@babel/plugin-syntax-async-generators"],
                         },
                     },
                     {
